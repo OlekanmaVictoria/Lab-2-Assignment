3e9288797a80638364ba6a04fa8bfb73bfa4ead6
OlekanmaVictoria <olekanmavictoria1994@gmail.com>
2022-04-02 14:12:47 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿String name  = "Victoria";
+Console.WriteLine(name);
+int avatar = 24;
+Console.WriteLine(avatar);
+long  naruto = 1600000;
+Console.WriteLine(naruto);
+float light = 4.7F;
+Console.WriteLine(light);
+double grace = 12.88D;
+Console.WriteLine (grace);
+bool isbackendcool = true;
+Console.WriteLine(isbackendcool);
+char myAssignment = 'B';
+Console.WriteLine(myAssignment);
+short wella = 20;
+Console.WriteLine(wellla);
+byte hope;
+decimal excellence =0.34M;
+Console.WriteLine(excellence);
+int val1 = 10;  
+double val2 =val1;double x = 1234.7;
+int a;
+a = (int)x;
+System.Console.WriteLine(a);
+
+
+
